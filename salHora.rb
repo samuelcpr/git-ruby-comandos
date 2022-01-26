03baa9858b2f5dfdcfb4b8e3093af22dc3464bf6
samuelcpr <samuelcpr464@gmail.com>
2022-01-25 23:22:19 -0300

    add

---
+++ salHora.rb
@@ -0,0 +1,9 @@
+puts "digite o valor que vc recebe"
+sal = gets.chomp.to_f
+puts "digite quantas horas vc trabalha pordia"
+horasTrabalho = gets.chomp.to_f
+x = sal/30
+y = x/horasTrabalho
+
+puts "voce ganha #{y}R$ por dia "
+
